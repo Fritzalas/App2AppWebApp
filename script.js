429a978b84fb4877d47af91b00f76c6f3b5cf3ce
FritzalasKosmas <k.fritzalas@mellongroup.com>
2025-08-13 14:45:10 +0300

    A second try to make a first communication.

--- script.js
+++ script.js
@@ -6,18 +6,25 @@ const tidText = document.getElementById('tidText');
 button.addEventListener('click', () => {
     tidText.textContent = 'tid:64999976';
 
+    const uriPrefix = `nbgpaytxn/`;
+
     const msg = `Performing get TID V2 transaction`;
     console.log(msg)
 
     // Construct the deeplink URI
     const appId = "WEB_INTENT"; // replace with actual BuildConfig.APPLICATION_ID
-    const uriPrefix = `intent://`; // or cosmotepaytxn://
     let uri = `request/v2?&TxnType=12&appId=${appId}`;
 
     // Encode URI
-    uri = encodeURIComponent(uri);
-    uri = uriPrefix + uri;
-
+    try {
+        uri = encodeURIComponent(uri);
+        uri = uriPrefix + uri;
+    } catch (e) {
+        console.log(e.message);
+    }
+    console.log("uri " + uri);
+    console.log("Sending txn request through intent");
+    uri = `intent://` + uri
     // Open URI using deeplink
     window.location.href = uri; // triggers Android app if installed
 });
